18fa2084617a78afefb327bf71c82290d68cc5e2
punkushal <spycymagar@gmail.com>
2025-05-28 13:18:15 +0545

    user provider functionality added

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:personal_emergency_assistant/models/user_profile.dart';
+import 'package:personal_emergency_assistant/services/storage_service.dart';
+import 'package:uuid/uuid.dart';
+
+//Provider for the Storage service
+final storageServiceProvider = Provider<StorageService>((ref) {
+  return StorageService();
+});
+
+//StateNotifier for userprofile
+class UserProfileNotifier extends StateNotifier<UserProfile?> {
+  final StorageService _storageService;
+  UserProfileNotifier(this._storageService) : super(null) {
+    //Load the user profile when initialzed
+    _loadUserProfile();
+  }
+
+  //Load the user profile from storage
+  Future<void> _loadUserProfile() async {
+    final profile = _storageService.getUserProfile();
+    state = profile;
+  }
+
+  //Save a new user profile
+  Future<bool> saveUserProfile({
+    required String name,
+    required String bloodGroup,
+    String allergies = '',
+    String medications = '',
+    String medicalConditions = '',
+  }) async {
+    //Create a new profile or update existing one
+    final newProfile =
+        state == null
+            ? UserProfile(
+              id: const Uuid().v4(),
+              name: name,
+              bloodGroup: bloodGroup,
+              medicalConditions: medicalConditions,
+              medications: medications,
+            )
+            : state!.copyWith(
+              name: name,
+              bloodGroup: bloodGroup,
+              allergies: allergies,
+              medicalConditions: medicalConditions,
+              medications: medications,
+            );
+
+    //Save to storage
+    final result = await _storageService.saveUserProfile(newProfile);
+
+    if (result) {
+      state = newProfile;
+    }
+    return result;
+  }
+
+  //Reset the user profile
+  Future<bool> resetUserProfile() async {
+    state = null;
+    return true;
+  }
+
+  //Provider for the UserProfileNotifier
+  final userProfileProvider =
+      StateNotifierProvider<UserProfileNotifier, UserProfile?>((ref) {
+        final storageService = ref.watch(storageServiceProvider);
+        return UserProfileNotifier(storageService);
+      });
+}
