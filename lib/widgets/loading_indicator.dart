d2fa7089c0a4bc4696d55ac23a1cf08994354c57
punkushal <spycymagar@gmail.com>
2025-05-29 14:35:41 +0545

    loading indicator widget created

---
+++ lib/widgets/loading_indicator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  final Color? color;
+  final double size;
+  final double strokeWidth;
+
+  const LoadingIndicator({
+    super.key,
+    this.color,
+    this.size = 24.0,
+    this.strokeWidth = 2.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(
+          color ?? Theme.of(context).primaryColor,
+        ),
+        strokeWidth: strokeWidth,
+      ),
+    );
+  }
+}
