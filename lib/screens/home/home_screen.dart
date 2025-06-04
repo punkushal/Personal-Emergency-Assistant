ae51e8d009df20cf3f3d1ef809b1e0e0a877319f
punkushal <spycymagar@gmail.com>
2025-06-04 17:25:49 +0545

    bottom navigation bar added

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:personal_emergency_assistant/constants/app_strings.dart';
+import 'package:personal_emergency_assistant/providers/contacts_provider.dart';
+import 'package:personal_emergency_assistant/screens/first_aid/first_aid_guide_screen.dart';
+import 'package:personal_emergency_assistant/screens/home/home_tab.dart';
+import 'package:personal_emergency_assistant/screens/settings/settings_screen.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -9,8 +14,40 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeTab(),
+    FirstAidGuideScreen(),
+    SettingsScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home screen..')));
+    ref.watch(emergencyContactsProvider);
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey[600],
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.medical_services),
+            label: AppStrings.firstAidTabLabel,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: AppStrings.settingsTabLabel,
+          ),
+        ],
+      ),
+    );
   }
 }
